3acdf246ddbf0db21cbcd38b42a1b1765637d981
Hakan Bayindir <hakan.bayindir@tubitak.gov.tr>
2022-05-06 21:26:17 +0300

    First refactoring run.
    - Added Eclipse PyDev project files to the repository.
    - Add an argument parser to the code to prepare for future
    functionality, and help info.
    - Change all variables to snake_case.
      - The properties coming from slurm are not changed. This is not a
    mistake.
    - Added `# -*- coding: utf-8 -*-` line to the python file.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,14 @@
 
 Global changelog file. Please use markdown formatting and write most recent changes on top. Dates use ISO formatting.
 
+## 20220506
+
+- Added Eclipse PyDev project files to the repository.
+- Add an argument parser to the code to prepare for future functionality, and help info.
+- Change all variables to snake_case.
+  - The properties coming from slurm are not changed. This is not a mistake.
+- Added `# -*- coding: utf-8 -*-` line to the python file.
+
 ## 20220428
 
 - Update `.gitignore` file.
--- src/lssrv.py
+++ src/lssrv.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 '''
 List free servers
@@ -20,7 +21,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 @author: Hakan Bayindir
 @contact: hakan.bayindir@tubitak.gov.tr
 @license: GNU/GPLv3
-@version: 0.0.4
+@version: 0.0.5a20220506
 '''
 
 import os
@@ -29,6 +30,7 @@ import subprocess
 
 from datetime import datetime
 
+import argparse
 import configparser
 
 # Rich related imports come here.
@@ -45,39 +47,39 @@ class Partition:
 		''' Default constructor for the class.'''
 		pass
 	
-	def __init__ (self, scontrolOutputString):
+	def __init__ (self, scontrol_output_string):
 		'''A constructor which gets the scontrol output and initializes the class.'''
 		
 		# Get a class wide logger.
 		logger = logging.getLogger('Partition_class')
 		
 		# and create an intermediate string for processing partition properties.
-		partitionPropertyBundle = scontrolOutputString.split(b' ')
+		partition_property_bundle = scontrol_output_string.split(b' ')
 		
 		# Will fill properties to a dictionary and will transfer to variables then.
-		for property in partitionPropertyBundle:
+		for property in partition_property_bundle:
 			
 			logger.debug('Processing feature: ' + str(property))
 			
 			# Extract the property name and value.
-			propertyNameAndValue = property.split(b'=')
+			property_name_and_value = property.split(b'=')
 			
 			# Directly create a new variable with the string coming from the property list.
-			setattr(self, propertyNameAndValue[0].decode('utf-8'), propertyNameAndValue[1].decode('utf-8'))
+			setattr(self, property_name_and_value[0].decode('utf-8'), property_name_and_value[1].decode('utf-8'))
 		
 		logger.debug('Extracted partition properties: ' + str(dir(self)))
 		
 		# Also, create partition load variables
-		self.pendingJobsPerCategory = dict()
-		self.pendingJobsPerCategory['Resources'] = 0 # We will query this anyway, and sometimes there are no pending jobs, so its absence creates problems.
-		self.pendingJobsTotal = 0 # Storing total here allows us to gain some important performance while showing the table.
-		self.busyCPUCount = 0
+		self.pending_jobs_per_category = dict()
+		self.pending_jobs_per_category['Resources'] = 0 # We will query this anyway, and sometimes there are no pending jobs, so its absence creates problems.
+		self.pending_jobs_total = 0 # Storing total here allows us to gain some important performance while showing the table.
+		self.busy_cpu_count = 0
 		
 		# Check whether partition has more than one type of servers.
 		# TODO: Fix this code. If there are two chunks of same server type, it makes mistakes.
-		serverTypes = self.Nodes.split(',')
+		server_types = self.Nodes.split(',')
 			
-		if len(serverTypes) > 1:
+		if len(server_types) > 1:
 			logger.debug('Partition has more than one server type.')
 			self.homogenous = False
 		else:
@@ -85,10 +87,10 @@ class Partition:
 			self.homogenous = True
 		
 
-def getAllPartitions():
+def get_all_partitions():
 	''' This functions ask Slurm for all partitions, then creates & returns several partition objects in a dictionary.'''
 	
-	logger = logging.getLogger('getAllPartitions')
+	logger = logging.getLogger('get_all_partitions')
 	
 	# Get the partitions directly from Slurm.
 	# Line is ending with \n. Always strip before splitting.
@@ -97,64 +99,73 @@ def getAllPartitions():
 	logger.info('Have read ' + str(len(partitions)) + ' partition(s).')
 	
 	# Create a list for storing partition objects.
-	partitionsDict = dict()
+	partitions_dictionary = dict()
 	
 	for partition in partitions:
-		tempPartition = Partition(partition)
-		partitionsDict[tempPartition.PartitionName] = tempPartition
+		temporary_partition = Partition(partition)
+		partitions_dictionary[temporary_partition.PartitionName] = temporary_partition
 	
 	logger.debug('Returning ' + str(len(partitions)) + ' partition(s).')
-	return partitionsDict
+	return partitions_dictionary
 
-def getJobStateForPartitions(partitions, partitionsToIgnore, queueStateFilePath):
+def get_job_state_for_partitions(partitions, partitions_to_ignore, queue_state_file_path):
 	''' This function gets all the jobs and updates the job state information per partition.'''
 	
-	logger = logging.getLogger('getJobStateForPartitions')
+	logger = logging.getLogger('get_job_state_for_partitions')
 	
 	# This is a very expensive command in terms of time. Calling it once and processing more is much more preferable to calling it 10+ times.
-	if os.path.exists(queueStateFilePath):
-		queueState = open(queueStateFilePath, 'r')
-		jobsInTheCluster = queueState.readlines()
-		queueState.close()
+	if os.path.exists(queue_state_file_path):
+		queue_state = open(queue_state_file_path, 'r')
+		jobs_in_the_cluster = queue_state.readlines()
+		queue_state.close()
 	
 	#TODO: Add exception handling here. Sometimes previous command returns nothing. 
-	for job in jobsInTheCluster:
-		splittedLine = job.split()
-		jobPartition = splittedLine[0].strip()
-		jobCoreCount = splittedLine[1].strip()
-		jobState = splittedLine[2].strip()
-		jobReason = splittedLine[3].strip()
+	for job in jobs_in_the_cluster:
+		splitted_line = job.split()
+		job_partition = splitted_line[0].strip()
+		job_core_count = splitted_line[1].strip()
+		job_state = splitted_line[2].strip()
+		job_reason = splitted_line[3].strip()
 
 		# If the partition is in the ignore list, just move along. There's nothing to see/do here.
-		if jobPartition in partitionsToIgnore:
+		if job_partition in partitions_to_ignore:
 			continue
 		
 		# Sometimes a user has no access to a partition, but we have the information anyways, and it creates problems.
-		if jobPartition not in partitions:
+		if job_partition not in partitions:
 			continue
 
 		# Parse the job state and fill relevant information accordingly.
-		if jobState == 'RUNNING':
-			logger.debug('Job is running with ' + jobCoreCount + 'core(s) on partition ' + jobPartition + '.')
-			partitions[jobPartition].busyCPUCount = partitions[jobPartition].busyCPUCount + int(jobCoreCount)
+		if job_state == 'RUNNING':
+			logger.debug('Job is running with ' + job_core_count + 'core(s) on partition ' + job_partition + '.')
+			partitions[job_partition].busy_cpu_count = partitions[job_partition].busy_cpu_count + int(job_core_count)
 		
-		elif jobState == 'PENDING':
-			logger.debug('Job is running with ' + jobCoreCount + 'core(s) on partition ' + jobPartition + ', because of ' + jobReason + '.')
+		elif job_state == 'PENDING':
+			logger.debug('Job is running with ' + job_core_count + 'core(s) on partition ' + job_partition + ', because of ' + job_reason + '.')
 			
 			# Let's check whether whether we already have a counter for the reason at hand. If not, create one.
-			if jobReason in partitions[jobPartition].pendingJobsPerCategory:
-				logger.debug('Incrementing key ' + jobReason + ' for partition ' + jobPartition + ' by 1.')
-				partitions[jobPartition].pendingJobsPerCategory[jobReason] = partitions[jobPartition].pendingJobsPerCategory[jobReason] + 1 
+			if job_reason in partitions[job_partition].pending_jobs_per_category:
+				logger.debug('Incrementing key ' + job_reason + ' for partition ' + job_partition + ' by 1.')
+				partitions[job_partition].pending_jobs_per_category[job_reason] = partitions[job_partition].pending_jobs_per_category[job_reason] + 1 
 			else:
-				logger.debug('Creating key ' + jobReason + ' for partition ' + jobPartition + ' and setting it to 1.')
-				partitions[jobPartition].pendingJobsPerCategory[jobReason] = 1
+				logger.debug('Creating key ' + job_reason + ' for partition ' + job_partition + ' and setting it to 1.')
+				partitions[job_partition].pending_jobs_per_category[job_reason] = 1
 				
 			# This needs to be incremented anyway:
-			partitions[jobPartition].pendingJobsTotal = partitions[jobPartition].pendingJobsTotal + 1
+			partitions[job_partition].pending_jobs_total = partitions[job_partition].pending_jobs_total + 1
 			
 if __name__ == '__main__':
+	 # Let's parse some arguments.
+	argument_parser = argparse.ArgumentParser()
+	argument_parser.description = 'A tool to see partitions\' state in the cluster.'
+
+	# Version always comes last.
+	argument_parser.add_argument ('-V', '--version', help='Print ' + argument_parser.prog + ' version and exit.', action='version', version=argument_parser.prog + ' version 0.0.5a20220506')    
+
+	arguments = argument_parser.parse_args()
+	
 	# Set up simple logging:
-	logging.basicConfig(filename = None, level=logging.ERROR)
+	logging.basicConfig(filename=None, level=logging.ERROR)
 	
 	logger = logging.getLogger('main')
 	
@@ -167,35 +178,35 @@ if __name__ == '__main__':
 		configuration.read('/etc/lssrv.conf')
 	
 		try:
-			partitionsToIgnore = configuration['Partitions']['partitions_to_hide'].strip().split()
+			partitions_to_ignore = configuration['Partitions']['partitions_to_hide'].strip().split()
 			
 		except KeyError as exception:
 			logger.debug('Cannot find configuration value for partitions to ignore, using defaults.')
-			partitionsToIgnore = list() # The default value is an empty list for that.	
+			partitions_to_ignore = list() # The default value is an empty list for that.	
 		
-		logger.debug('Got ' + str(len(partitionsToIgnore)) + ' partitions to ignore.')
-		logger.debug('Partition(s) to ignore: ' + str(partitionsToIgnore))
+		logger.debug('Got ' + str(len(partitions_to_ignore)) + ' partitions to ignore.')
+		logger.debug('Partition(s) to ignore: ' + str(partitions_to_ignore))
 		
 		try:		
-			queueStateFilePath = configuration['General']['squeue_state_file_path'].strip()
+			queue_state_file_path = configuration['General']['squeue_state_file_path'].strip()
 		except KeyError as exception:
 			logger.debug('Cannot find configuration value for squeue state file, using default.')
-			queueStateFilePath = '/var/cache/lssrv/squeue.state' # Use standard file path conventions, keep the system tidy.
+			queue_state_file_path = '/var/cache/lssrv/squeue.state' # Use standard file path conventions, keep the system tidy.
 			
-		logger.debug('squeue state file is: ' + str(queueStateFilePath))	
+		logger.debug('squeue state file is: ' + str(queue_state_file_path))	
 	else:
 		# Loading all the defaults automatically.
-		partitionsToIgnore = list() # The default value is an empty list for that.
-		queueStateFilePath = '/var/cache/lssrv/squeue.state' # Use standard file path conventions, keep the system tidy. 
+		partitions_to_ignore = list() # The default value is an empty list for that.
+		queue_state_file_path = '/var/cache/lssrv/squeue.state' # Use standard file path conventions, keep the system tidy. 
 		
 	
 	# Create a rich console.
 	console = Console()
 	
 	with console.status("Please wait while gathering resource information...", spinner = 'line'):
-		allPartitions = getAllPartitions()
-		getJobStateForPartitions(allPartitions, partitionsToIgnore, queueStateFilePath)
-		stateFileLastUpdateTime = os.path.getmtime(queueStateFilePath)
+		all_partitions = get_all_partitions()
+		get_job_state_for_partitions(all_partitions, partitions_to_ignore, queue_state_file_path)
+		stateFileLastUpdateTime = os.path.getmtime(queue_state_file_path)
 	
 	# Let's start building our table.
 	table = Table(title = 'TRUBA Partitions State')
@@ -212,23 +223,23 @@ if __name__ == '__main__':
 	table.add_column('RAM\nper Core')
 	
 	# These variables are created here to prevent re-creation in loop continuously.
-	coresPerNode = 0
+	cores_per_node = 0
 	
 	# This is where we add the information we have
-	for partitionName, partitionDetails in allPartitions.items():
+	for partition_name, partition_details in all_partitions.items():
 		
 		# If this partition is one of the ones we've been directed to ignore...
-		if partitionName in partitionsToIgnore:
+		if partition_name in partitions_to_ignore:
 			continue # Just go on, nothing to see here. 
 		
-		if partitionDetails.homogenous == True:
-			coresPerNode = str(int(int(partitionDetails.TotalCPUs) / int(partitionDetails.TotalNodes))) # Yes, we need all these conversions. All variables are stored as strings, and default Python division is double. So, yes.
+		if partition_details.homogenous == True:
+			cores_per_node = str(int(int(partition_details.TotalCPUs) / int(partition_details.TotalNodes))) # Yes, we need all these conversions. All variables are stored as strings, and default Python division is double. So, yes.
 		else:
-			coresPerNode = "-"
+			cores_per_node = "-"
 		
-		table.add_row(partitionName, str(int(partitionDetails.TotalCPUs) - partitionDetails.busyCPUCount), partitionDetails.TotalCPUs, str(partitionDetails.pendingJobsPerCategory['Resources']), str(partitionDetails.pendingJobsTotal), partitionDetails.TotalNodes, partitionDetails.MaxTime, partitionDetails.MinNodes, partitionDetails.MaxNodes, coresPerNode, partitionDetails.DefMemPerCPU + ' MB')
+		table.add_row(partition_name, str(int(partition_details.TotalCPUs) - partition_details.busy_cpu_count), partition_details.TotalCPUs, str(partition_details.pending_jobs_per_category['Resources']), str(partition_details.pending_jobs_total), partition_details.TotalNodes, partition_details.MaxTime, partition_details.MinNodes, partition_details.MaxNodes, cores_per_node, partition_details.DefMemPerCPU + ' MB')
 	
 	
 	console.print(table)
-	lastUpdateTimeMarkdown = Markdown('**Last update:** ' + datetime.fromtimestamp(stateFileLastUpdateTime).strftime('%Y-%m-%d %H:%M:%S'))
-	console.print(lastUpdateTimeMarkdown)
+	last_update_time_markdown = Markdown('**Last update:** ' + datetime.fromtimestamp(stateFileLastUpdateTime).strftime('%Y-%m-%d %H:%M:%S'))
+	console.print(last_update_time_markdown)
